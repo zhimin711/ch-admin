247e7b6d23984779dedcc84800adf4dde5050cfd
麻智敏 <ZhiMinMa@sf-express.com>
2019-10-29 17:50:47 +0800

    添加Kafka集群与主题管理

---
+++ src/api/kafka/topic.js
@@ -0,0 +1,55 @@
+import request from '@/utils/request2'
+const baseUrl = '/kafka/topic'
+export function list(query) {
+  return request({
+    url: `${baseUrl}/${query.page}/${query.limit}`,
+    method: 'get',
+    params: query.params
+  })
+}
+
+export function get(id) {
+  return request({
+    url: `${baseUrl}/${id}`,
+    method: 'get',
+    params: { id }
+  })
+}
+
+export function add(data) {
+  return request({
+    url: `${baseUrl}`,
+    method: 'post',
+    data
+  })
+}
+
+export function edit(id, data) {
+  return request({
+    url: `${baseUrl}/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+export function del(id) {
+  return request({
+    url: `${baseUrl}/${id}`,
+    method: 'delete'
+  })
+}
+
+export function getClusters() {
+  return request({
+    url: `${baseUrl}/clusters`,
+    method: 'get'
+  })
+}
+
+export function getTopics(clusterName, topicName) {
+  return request({
+    url: `${baseUrl}/topics`,
+    method: 'get',
+    params: { clusterName, topicName }
+  })
+}
---
+++ src/views/kafka/topic.vue
@@ -0,0 +1,258 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-input v-model="listQuery.params.clusterName" placeholder="集群名称" style="width: 200px;" class="filter-item" />
+      <el-input v-model="listQuery.params.topicName" placeholder="主题名称" style="width: 200px;" class="filter-item" />
+      <el-button v-if="checkPermission2(['KAFKA_TOPIC_SEARCH'])" class="filter-item" type="primary" icon="el-icon-search" @click="getList">
+        查询
+      </el-button>
+      <el-button class="filter-item" type="default" icon="el-icon-refresh" @click="listQuery.params = {}">
+        重置
+      </el-button>
+      <el-button v-if="checkPermission2(['KAFKA_TOPIC_ADD'])" class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="handleAdd">
+        添加主题
+      </el-button>
+    </div>
+    <el-table v-loading="listLoading" :data="listQuery.list" border fit highlight-current-row style="width: 100%">
+      <el-table-column width="120px" label="集群名称">
+        <template slot-scope="scope">
+          <span>{{ scope.row.clusterName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="主题名称">
+        <template slot-scope="scope">
+          <span>{{ scope.row.topicName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="存储类型">
+        <template slot-scope="scope">
+          <span>{{ scope.row.type }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="存储Jar包">
+        <template slot-scope="scope">
+          <span>{{ scope.row.classFile }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="存储对象">
+        <template slot-scope="scope">
+          <span>{{ scope.row.className }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="操作" width="120">
+        <template slot-scope="scope">
+          <el-link v-if="checkPermission2(['KAFKA_TOPIC_EDIT'])" type="primary" icon="el-icon-edit" @click="handleEdit(scope.row, scope.$index)">编辑</el-link>
+          <el-link v-if="checkPermission2(['KAFKA_TOPIC_DELETE'])" type="danger" icon="el-icon-delete" @click="handleDel(scope.row)">删除</el-link>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="listQuery.total>0" :total="listQuery.total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+
+    <el-dialog :visible.sync="dialogVisible" :title="dialogType==='edit'?'Edit 主题':'New 主题'">
+      <el-form :model="record" label-width="100px" label-position="left">
+        <el-form-item label="集群名称">
+          <!--<el-input v-model="record.clusterName" placeholder="集群名称" :disabled="dialogCodeEdit" />-->
+          <el-select v-model="record.clusterName" placeholder="请选择">
+            <el-option
+              v-for="item in options.clusters"
+              :key="item.clusterName"
+              :label="item.clusterName"
+              :value="item.clusterName">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="主题名称">
+          <!--<el-input v-model="record.topicName" placeholder="主题名称" />-->
+          <el-select
+            v-model="record.topicName"
+            filterable
+            remote
+            reserve-keyword
+            placeholder="请输入关键词"
+            :remote-method="remoteMethod"
+            :loading="loading" style="width:100%">
+            <el-option
+              v-for="item in options.topics"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="存储类型">
+          <el-input v-model="record.type" placeholder="存储类型" />
+        </el-form-item>
+        <el-form-item label="存储Jar包">
+          <el-input v-model="record.classFile" placeholder="存储Jar包" />
+        </el-form-item>
+        <el-form-item label="存储对象">
+          <el-input v-model="record.className" placeholder="存储对象" />
+        </el-form-item>
+      </el-form>
+      <div style="text-align:right;">
+        <el-button type="primary" @click="handleSubmit">保存</el-button>
+        <el-button type="danger" @click="dialogVisible=false">取消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+import { deepClone } from '@/utils'
+import { checkPermission2 } from '@/utils/permission' // 权限判断函数
+import { list, add, edit, del, getClusters, getTopics } from '@/api/kafka/topic'
+
+export default {
+  name: 'UserManager',
+  components: { Pagination },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      let s = 'draft'
+      if (status === '1') {
+        s = 'published'
+      } else if (status === '3') {
+        s = 'deleted'
+      }
+      return statusMap[s]
+    }
+  },
+  data() {
+    return {
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 10,
+        total: 0,
+        list: [],
+        params: {}
+      },
+      record: {},
+      dialogVisible: false,
+      dialogType: false,
+      dialogCodeEdit: false,
+      dialogVisible2: false,
+      loading: false,
+      options: {
+        clusters: [],
+        topics: []
+      }
+    }
+  },
+  created() {
+    this.getList()
+    this.getClusters()
+  },
+  methods: {
+    checkPermission2,
+    async getClusters() {
+      const resp = await getClusters()
+      if (resp && resp.success) this.options.clusters = resp.rows
+    },
+    getList() {
+      this.listLoading = true
+      list(this.listQuery).then(response => {
+        this.listQuery.list = response.rows
+        this.listQuery.total = response.total
+        this.listLoading = false
+      })
+    },
+    handleAdd() {
+      this.record = {}
+      this.dialogType = 'new'
+      this.dialogVisible = true
+      this.dialogCodeEdit = false
+    },
+    handleEdit(row) {
+      this.record = deepClone(row)
+      this.dialogType = 'edit'
+      this.dialogVisible = true
+      this.dialogCodeEdit = true
+    },
+    handleDel(row) {
+      const _this = this
+      this.$confirm('Confirm to remove the user?', 'Warning', {
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      })
+        .then(async() => {
+          await del(row.id)
+          _this.getList()
+          this.$message({
+            type: 'success',
+            message: 'Delete success!'
+          })
+        })
+        .catch(err => { console.error(err) })
+    },
+    async handleSubmit() {
+      const _this = this
+      let resp = null
+      let opName = '添加'
+      if (this.dialogType === 'new') {
+        resp = await add(this.record)
+      } else if (this.dialogType === 'edit') {
+        opName = '修改'
+        resp = await edit(this.record.id, this.record)
+      }
+      if (resp.success) {
+        this.dialogVisible = false
+        this.$notify({
+          title: `${opName}集群名称 Success!`,
+          dangerouslyUseHTMLString: true,
+          message: `
+            <div>集群名称: ${this.record.clusterName}</div>
+          `,
+          type: 'success'
+        })
+        _this.getList()
+      }
+    },
+    async remoteMethod(query) {
+      if (!this.record.clusterName || this.record.clusterName === '') {
+        this.$message({
+          type: 'warn',
+          message: '请先选择集群...'
+        })
+        return
+      }
+      if (query !== '') {
+        this.loading = true
+        getTopics(this.record.clusterName, query).then(response => {
+          this.loading = false
+          if (response.success) {
+            this.options.topics = response.rows.map(item => {
+              return { value: item, label: item }
+            })
+          }
+        })
+      } else {
+        this.options.topics = []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .edit-input {
+    padding-right: 100px;
+  }
+
+  .cancel-btn {
+    position: absolute;
+    right: 15px;
+    top: 10px;
+  }
+
+  .el-select .el-input__inner {
+    width: 360px;
+  }
+</style>
