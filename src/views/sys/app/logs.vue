37b0a237e1b3925fc79e4dca89ce5aee71f0ce62
麻智敏 <ZhiMinMa@sf-express.com>
2019-11-28 10:48:45 +0800

    日志搜索

--- src/views/sys/app/logs.vue
+++ src/views/sys/app/logs.vue
@@ -47,7 +47,7 @@
         <!--<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>-->
       </div>
       <div v-for="item in searchResults" :key="item.ip" class="text item">
-        <span>{{ item.ip }} - {{ item.dir }}</span>
+        <el-tag>{{ item.ip }} - {{ item.dir }}</el-tag>
         <el-collapse>
           <el-collapse-item v-for="e1 in item.records" :key="(item.ip + e1.fileName)" :title="e1.fileName" :name="e1.fileName">
             <div v-for="(e2,i) in e1.data" :key="(item.ip + e1.fileName + i)">
@@ -93,7 +93,7 @@ export default {
       options: {
         instances: []
       },
-      searchParams: {},
+      searchParams: { env: '2' },
       searchResults: [],
       activeNames: ['1'],
       record: {},
