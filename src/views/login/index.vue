da39857840ebf3495e900927447cfebb6ce1ca27
仙_梦如尘 <zhimin711@sina.com>
2020-09-09 07:48:34 +0800

    同步 vue admin template

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -172,17 +172,9 @@ export default {
         this.handleLogin()
       }
     },
-    checkCapslock({ shiftKey, key } = {}) {
-      if (key && key.length === 1) {
-        if (shiftKey && (key >= 'a' && key <= 'z') || !shiftKey && (key >= 'A' && key <= 'Z')) {
-          this.capsTooltip = true
-        } else {
-          this.capsTooltip = false
-        }
-      }
-      if (key === 'CapsLock' && this.capsTooltip === true) {
-        this.capsTooltip = false
-      }
+    checkCapslock(e) {
+      const { key } = e
+      this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
     },
     showPwd() {
       if (this.passwordType === 'password') {
