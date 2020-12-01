ce3c866c4590beae1946df571e9fde8a5b797613
麻智敏 <ZhiMinMa@sf-express.com>
2020-12-01 19:43:54 +0800

    mock

--- src/router/index.js
+++ src/router/index.js
@@ -147,10 +147,10 @@ export const exampleRoutes = [
         meta: { title: 'Error Log', icon: 'bug' }
       }
     ]
-  },
+  }
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
+  // { path: '*', redirect: '/404', hidden: true }
 ]
 
 /**
