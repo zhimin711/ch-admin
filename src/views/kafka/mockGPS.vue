367f8624a2dd1f6e617ec614dd04bf6defc0dac1
麻智敏 <ZhiMinMa@sf-express.com>
2020-12-16 19:47:52 +0800

    调整Mock GPS

--- src/views/kafka/mockGPS.vue
+++ src/views/kafka/mockGPS.vue
@@ -172,16 +172,43 @@
       title="提示"
       :visible.sync="dialogVisible"
       width="80%"
-    >
+    ><!--
+      <div>
+        <el-button type="primary" @click="addGPSPoint">确 定</el-button>
+        <el-button type="success" @click="getMapLine">获取规划路径</el-button>
+        <el-button @click="dialogVisible = false">关 闭</el-button>
+      </div>-->
       <div class="amap-page-container">
         <el-amap-search-box class="search-box" :search-option="searchOption" :on-search-result="onSearchResult" />
         <el-amap vid="amap" class="amap-demo" :zoom="12" :center="mapCenter" :events="events">
-          <el-amap-marker v-for="marker in markers" :position="marker" />
+          <el-amap-marker v-if="point" :position="point.pos" />
+          <el-amap-marker v-for="marker in markers" :position="marker.pos" :icon="marker.icon" />
         </el-amap>
       </div>
+      <div />
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addGPSPoint">确 定</el-button>
+        <el-button @click="cleanGPSPoints">清空轨迹点</el-button>
+        <el-button type="primary" @click="addGPSPoint(0)">设置为始发地</el-button>
+        <el-button type="primary" @click="addGPSPoint(-1)">设置为目的地</el-button>
+        <el-button type="primary" @click="addGPSPoint(1)">设置为经过点</el-button>
+        <el-button type="success" @click="getMapLine">获取规划路径</el-button>
+        <el-button @click="dialogVisible = false">关 闭</el-button>
+        <el-table
+          :data="markers"
+          style="width: 100%; margin-bottom: 10px;"
+        >
+          <el-table-column width="80" type="index" label="序号" />
+          <el-table-column prop="lng" label="经度" width="150" />
+          <el-table-column prop="lat" label="纬度" width="150" />
+          <el-table-column prop="addr" label="地址" />
+          <el-table-column align="center" label="操作" width="80">
+            <template slot-scope="scope">
+              <el-link type="danger" @click="handleDelGPSNode(scope.$index, scope.row)">
+                删除
+              </el-link>
+            </template>
+          </el-table-column>
+        </el-table>
       </span>
     </el-dialog>
   </div>
@@ -260,7 +287,7 @@ const rules2 = [
   { value: 'OBJECT', label: '对象', types: ['{}'], filterTypes: [] }
 ]
 export default {
-  name: 'KafkaGPSMock',
+  name: 'KafkaGPSMock1',
   data() {
     return {
       listLoading: false,
@@ -271,7 +298,9 @@ export default {
         threadSize: 4,
         batchSize: 10
       },
-      map: null,
+      map: { a: 'a' },
+      mapDriving: null,
+      mapRouteLine: null,
       geoCoder: null,
       markers: [
       ],
@@ -282,8 +311,8 @@ export default {
       mapCenter: [113.929208, 22.50641],
       events: {
         init: (o) => {
-          console.log(o.getCenter())
-          // console.log(this.$refs.map.$$getInstance())
+          // console.log(o.getCenter())
+          this.map = o
           o.getCity(result => {
             console.log(result)
           })
@@ -293,7 +322,7 @@ export default {
         'zoomchange': () => {
         },
         'click': (e) => {
-          this.addPos(e.lnglat.getLng(), e.lnglat.getLat())
+          this.markPoint(e.lnglat.getLng(), e.lnglat.getLat())
         }
       },
       gpsDates: [],
@@ -364,7 +393,7 @@ export default {
       let latSum = 0
       let lngSum = 0
       if (pois.length > 0) {
-        this.addPos(pois[0].lng, pois[0].lat)
+        this.markPoint(pois[0].lng, pois[0].lat)
         pois.forEach(poi => {
           const { lng, lat } = poi
           lngSum += lng
@@ -382,6 +411,7 @@ export default {
       this.dialogVisible = true
       this.markers = []
       this.point = null
+      this.mapRouteLine.setPath([])
     },
     initMap() {
       const _this = this
@@ -390,24 +420,87 @@ export default {
           // city: "010", //城市设为北京，默认：“全国”
           radius: 1000 // 范围，默认：500
         })
+        _this.mapDriving = new AMap.Driving({
+          policy: AMap.DrivingPolicy.LEAST_TIME, // 其它policy参数请参考 https://lbs.amap.com/api/javascript-api/reference/route-search#m_DrivingPolicy
+          ferry: 1, // 是否可以使用轮渡
+          province: '粤' // 车牌省份的汉字缩写
+        })
+
+        _this.mapRouteLine = new AMap.Polyline({
+          path: [],
+          isOutline: true,
+          outlineColor: '#ffeeee',
+          borderWeight: 2,
+          strokeWeight: 5,
+          strokeColor: '#0091ff',
+          lineJoin: 'round'
+        })
       })
     },
-    addPos(lng, lat) {
-      this.markers = [[lng, lat]]
-      this.point = {}
-      this.point.lng = lng
-      this.point.lat = lat
+    markPoint(lng, lat) {
+      const mark = { pos: [lng, lat] }
       const _this = this
-      this.geoCoder.getAddress(this.markers[0], function(status, result) {
+      this.geoCoder.getAddress(mark.pos, function(status, result) {
         if (status === 'complete' && result.regeocode) {
           const address = result.regeocode.formattedAddress
-          _this.point.addr = address
+          if (address === '中华人民共和国') {
+            _this.$message.warning('轨迹点无效，请重新选择轨迹点......')
+          } else {
+            mark.addr = address
+            mark.lng = lng
+            mark.lat = lat
+            _this.point = mark
+          }
         } else {
           console.error('根据经纬度查询地址失败')
         }
       })
     },
-    addGPSPoint() {
+    addGPSPoint(op) {
+      if (!this.point) {
+        this.$message.warning('请选择轨迹点......')
+        return
+      } else if (this.markers.length === 0 && op !== 0) {
+        this.$message.warning('请先设置始发点......')
+        return
+      }
+      const mark = Object.assign({}, this.point)
+      mark.type = op
+      if (op === 0) {
+        mark.icon = 'https://webapi.amap.com/theme/v1.3/markers/n/start.png'
+        if (this.markers.length > 0) {
+          if(this.markers[0].type === 0) {
+            this.markers[0].icon = 'https://webapi.amap.com/theme/v1.3/markers/n/mid.png'
+            this.markers[0].type = 1
+          }
+        }
+        this.markers = [mark].concat(this.markers)
+      } else if (op === -1) {
+        mark.icon = 'https://webapi.amap.com/theme/v1.3/markers/n/end.png'
+
+        const em = this.markers.pop()
+
+        if (em.type === -1) {
+          em.type = 0
+          em.icon = 'https://webapi.amap.com/theme/v1.3/markers/n/mid.png'
+        }
+        this.markers.push(em)
+        this.markers.push(mark)
+      } else {
+        mark.icon = 'https://webapi.amap.com/theme/v1.3/markers/n/mid.png'
+        const em = this.markers.pop()
+        if(em.type !== -1) {
+          this.markers.push(em)
+        }
+        this.markers.push(mark)
+        if (em.type === -1) this.markers.push(em)
+      }
+      this.point = null
+    },
+    cleanGPSPoints(){
+      this.markers = []
+    },
+    addGPSPoint2() {
       if (this.point) {
         this.gpsPositions.push({
           lng: this.point.lng,
@@ -429,6 +522,62 @@ export default {
         }
         this.subParams.push(item)
       })
+    },
+    getMapLine() {
+      const _this = this
+      if (this.markers.length < 2) {
+        this.$message.error('轨迹点不足，请添加轨迹点......')
+        return
+      }
+      const sp = new AMap.LngLat(this.markers[0].lng, this.markers[0].lat)
+      const ep = new AMap.LngLat(this.markers[this.markers.length - 1].lng, this.markers[this.markers.length - 1].lat)
+      const options = {
+        waypoints: []
+      }
+      if (this.markers.length > 2) {
+        for (let i = 1; i < this.markers.length - 1; i++) {
+          options.waypoints.push(new AMap.LngLat(this.markers[i].lng, this.markers[i].lat))
+        }
+      }
+      this.mapDriving.search(sp, ep, options, function(status, result) {
+        // result即是对应的驾车导航信息，相关数据结构文档请参考 https://lbs.amap.com/api/javascript-api/reference/route-search#m_DrivingResult
+        if (status === 'complete') {
+          if (result.routes && result.routes.length) {
+            // 绘制第一条路线，也可以按需求绘制其它几条路线
+            _this.drawRoute(result.routes[0])
+            console.log('绘制驾车路线完成')
+          }
+        } else {
+          console.error('获取驾车数据失败：' + result)
+        }
+      })
+    },
+    drawRoute(route) {
+      const path = this.parseRouteToPath(route)
+      console.log(path)
+      /* let routeLine = new AMap.Polyline({
+        path: path,
+        isOutline: true,
+        outlineColor: '#ffeeee',
+        borderWeight: 2,
+        strokeWeight: 5,
+        strokeColor: '#0091ff',
+        lineJoin: 'round'
+      })*/
+
+      this.mapRouteLine.setPath(path)
+      this.mapRouteLine.setMap(this.map)
+    },
+    parseRouteToPath(route) {
+      const path = []
+
+      for (let i = 0, l = route.steps.length; i < l; i++) {
+        const step = route.steps[i]
+        for (let j = 0, n = step.path.length; j < n; j++) {
+          path.push(step.path[j])
+        }
+      }
+      return path
     },
     async getClusters() {
       const resp = await getClusters()
