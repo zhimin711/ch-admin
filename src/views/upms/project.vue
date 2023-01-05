732ed4fab42f490db23cbfe05f877b6cf3837e7b
mazhimin <mazhimin@12301.cc>
2023-01-05 18:30:56 +0800

    tenant privilege

--- src/views/upms/project.vue
+++ src/views/upms/project.vue
@@ -40,7 +40,7 @@
             添加项目
           </el-button>
           <el-button
-            v-permission="'UPMS_PROJECT_ADD'"
+            v-permission="'UPMS_PROJECT_MEMBERS'"
             class="filter-item"
             style="margin-left: 10px;"
             type="primary"
